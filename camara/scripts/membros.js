7ffbe8ca53a8b7758b5f1f1c1a9637f6c2a68a8d
andrezasudmissao <andreza.sudmissao@gmail.com>
2026-09-14 16:28:27 -0300

    css cartão

--- camara/scripts/membros.js
+++ camara/scripts/membros.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/andrezasudmissao-sketch/wdd231/refs/heads/main/camara/dados/membros.json';
+const url = 'https://raw.githubusercontent.com/andrezasudmissao-sketch/wdd231/refs/heads/main/camara/dados.json/membros.json';
 
 const cartoes = document.querySelector('.cartoes-membros');
 
@@ -14,40 +14,52 @@ const exibirMembros = (membros) => {
     membros.forEach((membro) => {
 
         let cartao = document.createElement('section');
-        cartao.classList.add('cartao'); /*Bem mais simples do que criar cada cartão separadamente.*/
-
+        cartao.classList.add('cartao');
 
         let NomeDaEmpresa = document.createElement('h2');
-        let retrato = document.createElement('img');
-        let NumeroDetelefone = document.createElement('p');
-        let Nivel_de_associação = document.createElement('p');
-        let Produto_servico_oferecido = document.createElement('p');
-        let URL_do_site = document.createElement('a')
-       
-
-        // Nome da empresa
         NomeDaEmpresa.textContent = membro.NomeDaEmpresa;
 
-        // Imagem
-        retrato.setAttribute('src', `imagens/${membro.Nome_de_arquivo_de_imagem}`);
-        retrato.setAttribute('alt', `Logo da empresa ${membro.NomeDaEmpresa}`);
-        retrato.setAttribute('loading', 'lazy');
+
+        // contêiner principal (lado a lado)
+        let info = document.createElement('div');
+        info.classList.add('info');
+
+        // contêiner dos textos (em coluna)
+        let texto = document.createElement('div');
+        texto.classList.add('texto');
+
+        let retrato = document.createElement('img');
+        retrato.src = `imagens/${membro.Nome_de_arquivo_de_imagem}`;
+        retrato.alt = `Logo da empresa ${membro.NomeDaEmpresa}`;
+        retrato.loading = 'lazy';
+
+        let NumeroDetelefone = document.createElement('p');
         NumeroDetelefone.textContent = `Telefone: ${membro.NumeroDetelefone}`;
+
+        let Nivel_de_associação = document.createElement('p');
         Nivel_de_associação.textContent = `Nível de associação: ${membro.Nivel_de_associação}`;
+
+        let Produto_servico_oferecido = document.createElement('p');
         Produto_servico_oferecido.textContent = `Serviço: ${membro.Produto_servico_oferecido}`;
 
-        // Site
+        let URL_do_site = document.createElement('a');
         URL_do_site.href = membro.URL_do_site;
         URL_do_site.textContent = "Visitar site";
         URL_do_site.target = "_blank";
 
-        // Montar cartão
+        // textos em coluna
+        texto.appendChild(NumeroDetelefone);
+        texto.appendChild(Nivel_de_associação);
+        texto.appendChild(Produto_servico_oferecido);
+        texto.appendChild(URL_do_site);
+
+      
+        info.appendChild(texto);  
+        info.appendChild(retrato); 
+
+        // montar cartão
         cartao.appendChild(NomeDaEmpresa);
-        cartao.appendChild(retrato);
-        cartao.appendChild(NumeroDetelefone);
-        cartao.appendChild(Nivel_de_associação);
-        cartao.appendChild(Produto_servico_oferecido);
-        cartao.appendChild(URL_do_site);
+        cartao.appendChild(info);
 
         cartoes.appendChild(cartao);
     });
