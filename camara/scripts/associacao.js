2f76b1e1e1abb9b0aba8c1071a16738b7dcc2b2e
andrezasudmissao <andreza.sudmissao@gmail.com>
2026-09-24 11:40:13 -0300

    jason

--- camara/scripts/associacao.js
+++ camara/scripts/associacao.js
@@ -1,3 +1,25 @@
 
-const url = "";
+const url = "https://raw.githubusercontent.com/andrezasudmissao-sketch/wdd231/main/camara/dados/niveis.json";
 const associacao = document.querySelector("#contanier-niveis");
+
+async function obterDadosDeNiveis() {
+    const resposta = await fetch(url);
+    const dados = await resposta.json();
+    console.log(dados);
+
+        exibirNiveis(dados.niveis);
+     }
+
+
+obterDadosDeNiveis()
+
+const exibirNiveis = (niveis,) => {
+    niveis.forEach((nivel) => {
+
+        let cartao = document.createElement('section');
+        cartao.classList.add('cartao');
+
+        let  = document.createElement('h2');
+        NomeDaEmpresa.textContent = membro.NomeDaEmpresa;
+
+    })};
